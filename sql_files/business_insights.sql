a1af7e9719c50eb8cd6f74138495af95cf23c87e
farah-massuh <farahmassuh.dataanalyst@gmail.com>
2025-03-26 23:32:06 -0400

    answered bq 9

--- sql_files/business_insights.sql
+++ sql_files/business_insights.sql
@@ -174,3 +174,20 @@ SELECT
 FROM inventory i
 JOIN products p ON p.product_id = i.product_id
 WHERE i.stock < 10;
+
+
+
+/*----------
+9. Shipping Delays
+Identify orders where the shipping date is later than 2 days after the order date.
+Include customer, order details, and delivery provider.
+----------*/
+SELECT
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+    o.*,
+    s.shipping_providers,
+    (s.shipping_date - o.order_date) AS days_to_deliver
+FROM orders o
+JOIN customers c ON c.customer_id = o.customer_id
+JOIN shippings s ON o.order_id = s.order_id
+WHERE s.shipping_date - o.order_date > 2;
