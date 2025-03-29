6db8822f5ac57d809edabfc515e73abe454fa374
farah-massuh <farahmassuh.dataanalyst@gmail.com>
2025-03-28 20:09:44 -0400

    answered business query 10

--- sql_files/business_insights.sql
+++ sql_files/business_insights.sql
@@ -191,3 +191,19 @@ FROM orders o
 JOIN customers c ON c.customer_id = o.customer_id
 JOIN shippings s ON o.order_id = s.order_id
 WHERE s.shipping_date - o.order_date > 2;
+
+
+
+/*----------
+10. Payment Success Rate
+Calculate the percentage of successful payments across all orders.
+Include breakdowns by payment status (e.g., failed pending).
+----------*/
+SELECT
+    p.payment_status,
+    COUNT(*) AS total_payments,
+    ROUND(COUNT(*) * 100 / SUM(COUNT(*)) OVER(), 2) AS percentage
+FROM orders o
+JOIN payments p ON o.order_id = p.order_id
+GROUP BY 1
+ORDER BY 3 DESC;
