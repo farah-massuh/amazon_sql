637fdd5543cdfebbbeeedbe6d3803efa578101ea
farah-massuh <farahmassuh.dataanalyst@gmail.com>
2025-03-30 16:12:22 -0400

    answered bq 16

--- sql_files/business_insights.sql
+++ sql_files/business_insights.sql
@@ -353,3 +353,20 @@ FROM (
     ORDER BY 1
 )
 WHERE rank <= 5;
+
+
+
+/*----------
+16. Revenue by Shipping Provider
+Calculate the total revenue handled by each shipping provider.
+Include the total number of orders handled and the average delivery time for each provider
+----------*/
+SELECT
+    s.shipping_providers,
+    COUNT(o.order_id) orders_handled,
+    ROUND(SUM(oi.total_sale)) total_sale,
+    ROUND(COALESCE(AVG(s.return_date - s.shipping_date), 0)) as average_days
+FROM orders o
+JOIN order_items oi ON oi.order_id = o.order_id
+JOIN shippings s ON s.order_id = o.order_id
+GROUP BY 1
