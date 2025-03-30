0f187cf6fd719fd27f060bbc6acee3b1caa19f43
farah-massuh <farahmassuh.dataanalyst@gmail.com>
2025-03-30 15:58:53 -0400

    answered bq 14

--- sql_files/business_insights.sql
+++ sql_files/business_insights.sql
@@ -309,3 +309,24 @@ FROM (
 )
 ORDER BY return_percentage DESC
 LIMIT 10;
+
+
+
+/*----------
+14. Identify customers either returning or new
+If the customer has done more than 5 returns categorize them as returning, otherwise, new
+List name, total orders, total returns
+----------*/
+SELECT
+    *,
+    CASE WHEN total_returns > 5 THEN 'Returning' ELSE 'New' END customer_type
+FROM (
+    SELECT
+        CONCAT(c.first_name, ' ', c.last_name) full_name,
+        COUNT(o.order_id) total_orders,
+        SUM(CASE WHEN o.order_status = 'Returned' THEN 1 ELSE 0 END) total_returns
+    FROM orders o
+    JOIN customers c ON c.customer_id = o.customer_id
+    JOIN order_items oi ON oi.order_id = o.order_id
+    GROUP BY 1
+)
