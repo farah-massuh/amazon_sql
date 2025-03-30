b3fa6cfdbd42a156c1f7fec4e350942fc210f8c6
farah-massuh <farahmassuh.dataanalyst@gmail.com>
2025-03-30 15:59:17 -0400

    answered bq 15

--- sql_files/business_insights.sql
+++ sql_files/business_insights.sql
@@ -330,3 +330,26 @@ FROM (
     JOIN order_items oi ON oi.order_id = o.order_id
     GROUP BY 1
 )
+
+
+
+/*----------
+15. Top 5 Customers by orders in Each state
+Identify the top 5 customers with the highest number of orders for each state.
+Include the number of orders and total sales for each customer
+----------*/
+SELECT *
+FROM (
+    SELECT
+        c.state,
+        CONCAT(c.first_name, ' ', c.last_name) customers,
+        COUNT(o.order_id) total_orders,
+        SUM(oi.total_sale) total_sale,
+        DENSE_RANK() OVER(PARTITION BY c.state ORDER BY COUNT(o.order_id) DESC) as rank
+    FROM orders o
+    JOIN order_items oi ON oi.order_id = o.order_id
+    JOIN customers c ON c.customer_id = o.customer_id
+    GROUP BY 1, 2
+    ORDER BY 1
+)
+WHERE rank <= 5;
